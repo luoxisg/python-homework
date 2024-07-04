0457bc03cfc05d2bc5c62dca6cc478b0e9d47d37
luoxisg <49471150+luoxisg@users.noreply.github.com>
2024-07-04 12:42:02 +0800

    Add files via upload

---
+++ 04 compare .py
@@ -0,0 +1,10 @@
+# 4-> Write a program to input 2 int numbers, a and b. Print True if a greater than or equal to b. If not, print False.
+
+a = input("please input a  ")
+b = input('please input b  ')
+
+if a >= b:
+    print ("True")
+else:
+    print ("False")
+    
