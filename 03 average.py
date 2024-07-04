1529d19447f322e8de8010dec33fded6645d326b
luoxisg <49471150+luoxisg@users.noreply.github.com>
2024-07-04 12:36:27 +0800

    Add files via upload

---
+++ 03 average.py
@@ -0,0 +1,6 @@
+# 3-> Write a program to input 2 floating point numbers & print their average.
+
+a = float(input ("This is first floating number"))
+b = float(input ("This is 2nd floating number"))
+
+print ("the average is  " + str((a+b)/2))
