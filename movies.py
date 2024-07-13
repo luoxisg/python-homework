cbf1d0a6205d5585a9873af13e6ba5c3bf9eb252
luoxisg <49471150+luoxisg@users.noreply.github.com>
2024-07-13 10:58:13 +0800

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,8 @@
+#  Write a program to ask the user to enter names of their 3 favourite movies & store them in a list.
+first_Movie = input('PLEASE INPUT THE FIRST NAME OF MOVIE')
+second_Movie = input('PLEASE INPUT THE SECOND NAME OF MOVIE')
+third_Movie = input('PLEASE INPUT THE THIRD NAME OF MOVIE')
+
+list_moive = [first_Movie,second_Movie,third_Movie]
+
+print (list_moive)
