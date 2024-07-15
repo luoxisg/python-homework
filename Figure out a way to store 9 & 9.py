194ee9540b705e9869d0fb111318ea9b1eadd66f
luoxisg <49471150+luoxisg@users.noreply.github.com>
2024-07-15 16:37:51 +0800

    Add files via upload

---
+++ Figure out a way to store 9 & 9.py
@@ -0,0 +1,8 @@
+# Figure out a way to store 9 & 9.0 as separate values in the set. (You can take help of built-in data types) 
+
+value_set = set()
+
+value_set.add((9,'int'))
+value_set.add((9.0,'float'))
+
+print ("value set : ", value_set)
